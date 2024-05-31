c2115c1b709c4e62a3048baca0cd20e4ccd74a7f
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-05-31 12:54:51 +0530

    Added files

---
+++ Strings/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; // Use the standard namespace
+
+int main() {
+    char temp;
+    temp='A';
+    cout<<temp;   
+    char name[10]={'J','O','H','N'};
+    cout<<name;
+    string n="Anubhav";
+    cout<<n;
+}
