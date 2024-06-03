9fc6c266e97a50eeb51f70ba4fe8df7a8963c0cc
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-06-03 12:45:47 +0530

    Create display.cpp

---
+++ Linked List/display.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n)
+{
+    int i;
+    struct Node *last, *t;
+    first=new Node;
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(int i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t; 
+        last=t;
+    }
+}
+
+void Display()
+{
+    struct Node *p;
+    p=first;
+    while(p!=NULL)
+    {
+        cout<<p->data<<endl;
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int A[]={4,5,8,9,7};
+    create(A,5);
+    Display();
+
+    return 0;
+}
